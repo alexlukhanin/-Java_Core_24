3fd4d1d03d92eee17c703bad923ad5b60508165f
alexl <alexl@Lukhanin>
2020-07-05 02:09:03 +0300

    release 1.0

---
+++ hw24/src/ua/lviv/lgs/Days.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package ua.lviv.lgs;
+
+/**
+ * @author alexl
+ *
+ */
+public enum Days {
+	MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
---
+++ hw24/src/ua/lviv/lgs/MyWrognInputException.java
@@ -0,0 +1,15 @@
+package ua.lviv.lgs;
+
+public class MyWrognInputException extends Exception {
+
+	public MyWrognInputException(String string) {
+		System.out.println(string);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+
+}
